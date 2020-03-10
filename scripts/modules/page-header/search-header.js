8f12ab23f7afa99d2b28068bf64af97b566fd60e
dep_jitender <jitender@deplabs.com>
2020-03-10 17:05:56 +0530

    GTINGM-330 FED Global sticky header click outside on mobile

--- scripts/modules/page-header/search-header.js
+++ scripts/modules/page-header/search-header.js
@@ -2,12 +2,21 @@ define([
     'modules/jquery-mozu'
 ], function($) {
 
-    $(".mz-searchbox-button-top").click( function (){
-		$("#searchbox").toggle();
+	$(document).on('click', ".mz-pageheader.affix .mz-searchbox-button-top", function (){
+		$("#searchbox").show();			
+	});
+	$(document).mouseup(function(e) 
+	{
+		var container = $("#searchbox");
+
+		if (!container.is(e.target) && container.has(e.target).length === 0 && ($(window).width() < 495)) 
+		{
+			container.hide();
+		}
 	});
 	$(window).on('resize', function() {
 		if($(window).width() > 495){
-			$("#searchbox").css("display", "block");
+			$("#searchbox").show();
 		}
 	});
 
