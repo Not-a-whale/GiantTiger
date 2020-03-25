320ca3db41b308cd7dc82fce273a6968dcfa86db
dep_jitender <jitender@deplabs.com>
2020-03-25 22:04:52 +0530

    GTINGM-123 FED Homepage today deals carousel widget

---
+++ scripts/modules/product/product-carousel-swiper.js
@@ -0,0 +1,40 @@
+﻿define([
+    'modules/jquery-mozu',
+    "swiper"
+], function($, Swiper) {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        spaceBetween: 0,
+        loop: false,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev'
+        },
+        preventClicks: false,
+        preventClicksPropagation: false,
+        breakpoints: {
+            1024: {
+                slidesPerView: 3
+            },
+            992: {
+                slidesPerView: 2
+            },
+            768: {
+                slidesPerView: '1'
+            },
+            640: {
+                slidesPerView: '1'
+            },
+            320: {
+                slidesPerView: '1'
+            }
+        }
+    });
+    function window_resize(){
+    }
+    $(window).resize(function() {
+        window_resize();
+    });
+
+
+});
